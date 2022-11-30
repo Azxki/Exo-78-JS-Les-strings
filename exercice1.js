a022b5285b1f2363cc705ed738f1b7a4e0e11206
azxki <cococoursan@gmail.com>
2022-11-30 15:05:19 +0100

    EXO

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,21 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
-// Completer le code suivant pour afficher la longueur de la variable monTexte
+let position = monTexte.indexOf('i');
+console.log(position);
 
-alert(monTexte.);
+alert(monTexte.length);
 
+document.getElementById('position').innerHTML = position;
 
-//Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let stock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+let upper = stock.toUpperCase();
 
+console.log(upper);
+document.getElementById('chaineMaj').innerHTML = upper;
 
-
-
-
+for (i = 0; i < stock.indexOf('a'); i++) {
+    alert("lettre a trouvé");
+}
 
 
 
